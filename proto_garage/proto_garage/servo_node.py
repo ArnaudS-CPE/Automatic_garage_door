e6465f5f787a45e53dbcac383a75b7c9aa78700c
MeasterEgg <tom.reche@cpe.fr>
2024-11-15 09:57:58 +0100

    servo

--- proto_garage/proto_garage/servo_node.py
+++ proto_garage/proto_garage/servo_node.py
@@ -12,23 +12,18 @@ class ServoNode(Node):
         super().__init__('servo_node')
         self.command_subscriber = self.create_subscription(String, 'door_control_command', self.action_porte ,10)
         self.car_coming_in = self.create_publisher(Empty, 'car_coming_in', 10)
-        self.servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
-        self.servo.angle = 90
+        self.servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023,initial_angle=90)
 
 
     def action_porte(self,msg):
         msgtof=Empty()
         if msg.data=="open":
             self.get_logger().info('Door opening')
-            for i in range(100):
-                time.sleep(0.05)
-                self.servo.angle = 90*(i/100)
+            self.servo.angle = 90
             self.car_coming_in.publish(msgtof)
         elif msg.data=="close":
             self.get_logger().info('Closing the door')
-            for i in range(100):
-                time.sleep(0.05)
-                self.servo.angle = 90-(90*(i/100))
+            self.servo.angle = 0
 
 def main(args=None):
     rclpy.init(args=args)
